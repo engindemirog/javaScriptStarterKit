6c2118aadf2aec209d9b824a02802b2c743525fa
erdin.fidan <erdin.fidan@yesilirmakedas.com>
2021-10-19 00:02:19 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log("Merhaba Kodlama.io")
+
+var kiloDun=101.50
+var kiloBugun=95.10
+
+console.log(kiloDun)
+
+let sporHareketler=["eliptik","yüzme","sprint","omuz","üst kol","bacak"]
+
+console.log(sporHareketler)
+console.log("<ul>")
+sporHareketler.forEach(x => {
+    console.log("<li>"+x+"</li>")    
+});
+console.log("<ul>")
