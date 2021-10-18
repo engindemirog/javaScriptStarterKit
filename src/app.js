30862d09d018eecf03beb5fb5de01cc2649ecbfd
AO <ahmet_ozer@hotmail.com>
2021-10-19 00:02:41 +0300

    19.10.2021

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 console.log("Merhaba Kodlama.io")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
+    
+}
+console.log("</ul>")
+console.log(konutKredileri)
