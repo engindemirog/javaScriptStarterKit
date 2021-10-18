95b376095ddad08652d426118f1bfd294a9655d5
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2021-10-19 00:04:23 +0300

    18Ekim2021

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log("merhaba kodlama.io")
+let konutKredisi = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutKredisi.length; i++) {
+    console.log("<li>" + konutKredisi[i] + "<li>")
+
+}
+console.log("<ul>")
