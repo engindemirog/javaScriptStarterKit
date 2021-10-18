b5e30b6411be8d3488b39b84d520c6618ca30b04
hasanozt <ozturktr60@gmail.com>
2021-10-19 00:02:13 +0300

    18ekimDersSonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+//console.log("merhaba kodlama.io");
+
+//let dolarDun="ankara"
+//let dolarBugun=9.30
+
+//let dolarDun=9.10
+//console.log(dolarDun)
+
+let konutKredileri=["konut k","emlak k", "kamu konut k"]
+
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length; i++)
+{
+    console.log("<li>"+konutKredileri[i]+"</>")
+}
+console.log("<ul>")
+
+//console.log(konutKredileri)
+
