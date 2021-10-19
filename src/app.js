20b218f3c27bdbc71bab2a828354d87e36fec161
osman ersoy <osman_00_09@hotmail.com>
2021-10-19 18:25:41 +0300

    18 ekim ders

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun =11 // Eklenemiyor hata verir const da
+ 
+console.log(euroDun)
+
+//array
+
+let konutKrediler = ["Konut Kredisi","Emlak konut Kredisi","Kamu Konut Kredisi"]
+
+let users = getUsersFromApi();
+for     
+console.log(konutKrediler)
