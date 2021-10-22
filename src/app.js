db7c1e12afa237bccde31489ea90dee26dc8fd6d
berkaynayman <berkaynayman4@gmail.com>
2021-10-22 15:48:44 +0300

    day-1

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Hello Berkay Nayman")
+// https://jsonplaceholder.typicode.com/
+var dolarDun   = 9.20
+dolarDun = "Ankara"
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+/*let dolarDun   = 9.20 yukarda yazdırdığımız için undefined hatası
+alırız yani tanımlanmamış */
+
+const euroDun = 11.2
+//euroDun = 11
+console.log(euroDun)
+
+//array
+//isimlendirmede camelCase
+//PascalCasing
+let konutKredileri = ['konut kredisi', 'emlak konut kredisi', 'kamu konut kredisi']
+
+for(let i = 0 ; i<konutKredileri.length ; i++){
+    console.log(konutKredileri[i])
+}
+
+console.log(konutKredileri)
