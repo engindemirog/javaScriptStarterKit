a0feca2f783be122cb74661de32f2b5d3d58217d
Emirhanns <92988123+Emirhanns@users.noreply.github.com>
2021-10-28 12:33:41 +0300

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,28 @@
+let dolarDun = 9.12
+let dolarBugun = 9.25
+
+{
+   let dolarDun = 9.10  
+}
+
+console.log(dolarDun)
+ 
+const euroDun = 11.2
+//euroDun = 11 
+
+
+console.log(euroDun)
+
+//array
+
+let konutKredileri = ["konut kredisi", "emlak konut kredisi", "kamu konut kredisi", "Özel konut kredisi"]
+  
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+   console.log("<li>" +konutKredileri[i]+ "/<li>")
+}
+console.log("/<ul>")
+
+console.log(konutKredileri)
+
+
