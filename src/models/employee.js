8a7773b9ed4025e9259f38c1f245c7fbd4f28606
zgnzdn <41526772+zgnzdn@users.noreply.github.com>
2022-03-14 00:51:12 +0300

    14 Mart Ders Sonu

--- src/models/employee.js
+++
@@ -1,8 +0,0 @@
-import User from "./user.js"
-
-export default class Employee extends User{
-    constructor(id, firstName, lastName, city,age, salary) {
-        super(id, firstName, lastName, city,age)
-        this.salary = salary
-    }
-}
